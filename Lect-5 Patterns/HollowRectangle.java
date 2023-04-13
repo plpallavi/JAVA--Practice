f3dc8cb0000024fecc6e7339da74885ded881505
plpallavi <pallavi121202@gmail.com>
2023-04-13 23:56:06 +0530

    JAVA

---
+++ Lect-5 Patterns/HollowRectangle.java
@@ -0,0 +1,22 @@
+// Print the pattern - Hollow Rectangle
+// *****************
+// *               *
+// *               *
+// *               *
+// *****************
+public class HollowRectangle {
+    public static void main(String[] args) {
+        int m = 5;
+        int n = 10;
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == 1 || j == 1 || i == m || j == n) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ Lect-5 Patterns/RotatedInvertedHalfPyramid.java
@@ -0,0 +1,13 @@
+// Print the pattern Half Pyramid
+//    *
+//   **
+//  ***
+// ****
+public class RotatedInvertedHalfPyramid {
+    public static void main(String[] args){
+        int n=5;
+        
+        for(int i=1; )
+
+    }
+}
