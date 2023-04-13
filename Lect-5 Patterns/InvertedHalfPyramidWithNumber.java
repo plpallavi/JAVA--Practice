6d63e0b4273463195e8b910b76e6d0c5ce026fcf
plpallavi <pallavi121202@gmail.com>
2023-04-14 03:06:52 +0530

    JAVA

---
+++ Lect-5 Patterns/InvertedHalfPyramidWithNumber.java
@@ -0,0 +1,19 @@
+public class InvertedHalfPyramidWithNumber {
+    public static void main(String agrs[]) {
+        int n = 5;
+        // for (int i = n; i >= 1; i--) {
+        // for (int j = 1; j <= i; j++) {
+        // System.out.print(j + " ");
+        // }
+        // System.out.println();
+        // }
+
+        // 2nd approach
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i + 1; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
