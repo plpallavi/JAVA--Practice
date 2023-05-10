ca47f245c8fefbb7f2ecc37b40507c9c7609d0bd
plpallavi <pallavi121202@gmail.com>
2023-05-11 00:02:05 +0530

    JAVA

--- Lect-27 LinkedList/linkedList.java
+++ Lect-27 LinkedList/linkedList.java
@@ -1,3 +1,4 @@
+
 //Properties
 // --> Variable Size
 // --> Non- contiguous Memory
@@ -5,7 +6,67 @@
 // --> Search in O(n)
 
 public class linkedList {
-    public static void main(String[] args) {
+    Node head;
 
+    class Node {
+        String data;
+        Node next;
+
+        Node(String data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    // add - first, last
+    public void addFirst(String data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public void addLast(String data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node currNode = head;
+        while (currNode.next != null) {
+            currNode = currNode.next;
+        }
+
+        currNode.next = newNode;
+    }
+
+    // print
+    public void printList() {
+        if (head == null) {
+            System.out.println("List is empty");
+            return;
+        }
+        Node currNode = head;
+        while (currNode != null) {
+            System.out.print(currNode.data + "-> ");
+            currNode = currNode.next;
+        }
+        System.out.println("NULL");
+    }
+
+    public static void main(String[] args) {
+        linkedList list = new linkedList();
+        list.addFirst("a");
+        list.addFirst("is");
+        list.printList();
+
+        list.addLast("List");
+        list.printList();
+
+        list.addFirst("this");
+        list.printList();
     }
 }
