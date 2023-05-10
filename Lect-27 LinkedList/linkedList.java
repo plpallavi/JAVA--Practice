88eff991e0bedbce49fa20b527ef52339e60c7e5
plpallavi <pallavi121202@gmail.com>
2023-05-10 23:34:03 +0530

    Java

--- Lect-27 LinkedList/linkedList.java
+++ Lect-27 LinkedList/linkedList.java
@@ -1,5 +1,11 @@
-public class linkedList{
+//Properties
+// --> Variable Size
+// --> Non- contiguous Memory
+// -->Insert in O(1)
+// --> Search in O(n)
+
+public class linkedList {
     public static void main(String[] args) {
-        
+
     }
 }
