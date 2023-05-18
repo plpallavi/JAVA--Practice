b18e3e58fe3eed0c93833ab92afc7e8133218c71
plpallavi <pallavi121202@gmail.com>
2023-05-19 03:41:54 +0530

    JAVA

--- Lect-29 QuesOfLinkedList/palindrom.java
+++ Lect-29 QuesOfLinkedList/palindrom.java
@@ -2,6 +2,50 @@
 //Palindrom : eg. POP, BOB, 121, 12321
 
 public class palindrom {
+    public ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        List curr = head;
+
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+
+    public ListNode findMiddle(ListNode head) {
+        ListNode hare = head;
+        ListNode turtle = head;
+
+        while (hare.next != null && hare.next.next != null) {
+            hare = hare.next.next;
+        }
+
+        return turtle;
+    }
+
+    public boolean isPalindrom(ListNode head) {
+        if (head == null || head.next == null) {
+            return true;
+        }
+
+        ListNode middle = findMiddle(head);
+        ListNode secondHalfStart = reverse(middle.next);
+
+        ListNode firstHalfStart = head;
+        while (secondHalfStart != null) {
+            if (firstHalfStart.val != secondHalfStart.val) {
+                return false;
+            }
+
+            firstHalfStart = firstHalfstart.next;
+            secondHalfStart = secondHalfStart.next;
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
 
     }
