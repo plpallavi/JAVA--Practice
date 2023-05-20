f438b97af3a6108f559bd6cace04706b80a0124d
plpallavi <pallavi121202@gmail.com>
2023-05-20 18:42:33 +0530

    JAVA

---
+++ Lect-29 QuesOfLinkedList/hasCycleLinkedList.java
@@ -0,0 +1,25 @@
+public class hasCycleLinkedList {
+    public boolean hasCycle(ListNode head) {
+
+        if (head == null) {
+            return false;
+        }
+        ListNode hare = head; // fast
+        ListNode tuttle = head; // slow
+
+        while (hare.next != null && hare.next != null) {
+            hare = hare.next.next.next;
+            turtle = turtle.next;
+
+            if (hare == turtle) {
+                return true;
+            }
+
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
