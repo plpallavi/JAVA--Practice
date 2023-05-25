d4c786f1df9ee7049cdcc05692b4c534fe58628a
plpallavi <pallavi121202@gmail.com>
2023-05-25 22:52:33 +0530

    JAVA

--- Lect-30 Stack/stackUsingLinkedList.java
+++ Lect-30 Stack/stackUsingLinkedList.java
@@ -2,8 +2,8 @@
 //-- Push (TC_> O(1))
 //-- Pop (TC_> O(1))
 //-- Peek (TC_> O(1))
-public class stack {
+public class stackUsingLinkedList {
     public static void main(String[] args) {
-        
+
     }
 }
