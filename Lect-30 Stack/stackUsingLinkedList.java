1eae5e41ff45e2f82193bcf25d5ffd7d93ea1b03
plpallavi <pallavi121202@gmail.com>
2023-05-26 20:50:30 +0530

    JAVA

--- Lect-30 Stack/stackUsingLinkedList.java
+++ Lect-30 Stack/stackUsingLinkedList.java
@@ -2,7 +2,24 @@
 //-- Push (TC_> O(1))
 //-- Pop (TC_> O(1))
 //-- Peek (TC_> O(1))
+// Stack can be implemented using array, linkedlist and arrayList
+
 public class stackUsingLinkedList {
+
+    static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            next = null;
+        }
+    }
+
+    static class Stack {
+
+    }
+
     public static void main(String[] args) {
 
     }
