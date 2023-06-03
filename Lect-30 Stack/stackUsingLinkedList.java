d0fe4884b7563db0d40e3f0d5b675bbbd07b4102
plpallavi <pallavi121202@gmail.com>
2023-06-03 14:12:19 +0530

    JACA

--- Lect-30 Stack/stackUsingLinkedList.java
+++ Lect-30 Stack/stackUsingLinkedList.java
@@ -38,11 +38,33 @@ public class stackUsingLinkedList {
 
             public static int pop(){
 
+                if(isEmpty()){
+                    return -1;
+                }
+                int top = head.data;
+                head = head.next;
+                return top;
+            }
+
+            public static int peek(){
+                if(isEmpty()){
+                    return -1;
+                }
+                return head.data;
             }
         }
     }
 
     public static void main(String[] args) {
+        Stack s = new Stack();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        s.push(4);
 
+        while (!s.Empty()) {
+            System.out.println(s.peek());
+            s.pop();
+        }
     }
 }
